57fbab3e49c295c6d85e98e30e75dfb86faf5576
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2024-05-07 22:15:29 +0200

    backend_MVC_posts: was implemented the media files uploads  using firebase cloud, for the posts model

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,11 @@
 import postModel from "../models/post.js";
+import config from "../firebase/firebase.js"
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { memory } from "../middelware/uploadMedia.js";
+
+
+
 
 export const getLastPosts = async(req,res)=> {
     try {
@@ -9,24 +16,43 @@ export const getLastPosts = async(req,res)=> {
     }
 }
 
+initializeApp(config.firebaseConfig);
+const storage = getStorage();
 export const createPost = async(req,res)=> {
 
-     const {player_id,title,description,date,category} =req.body;
+    
     try {
-        const newPost = new postModel({
-            // player_id : player_id,
-            file: req.file.filename,
-            title: title,
-            description: description,
-            date: date,
-            category: category,
+
+        await memory.single('file')(req,res, async (err)=> {
+            if(err){
+                console.error('Error al cargar el archivo en memoria:', err);
+                return res.status(500).json({ message: 'Error al cargar el archivo en memoria' });
+            }
+
+            const fileBuffer = req.file.buffer;
+            
+
+            const tempFileRef = ref(storage,  `media/${req.file.originalname}`);
+
+            await uploadBytes(tempFileRef,fileBuffer)
+
+            const tempFileDownloadUrl = await getDownloadURL(tempFileRef);
+
+            const {users_id,title,description,date,category} =req.body;
+            const newPost = new postModel({
+                users_id: users_id,
+                file: tempFileDownloadUrl, // Guardar la URL del archivo temporal en la base de datos
+                title: title,
+                description: description,
+                category: category,
+            }); 
+
+            await newPost.save();
+            return res.status(200).json({ message: 'Post creado correctamente', newPost });
         })
+    } catch(error) {
+        console.error('Error al crear el post:', error);
+        return res.status(500).json({ message: 'Error al crear el post' });
+    }
 
-        await newPost.save()
-        return res.status(200).json({message: 'Post creado correctamente', newPost});
-
-        }catch (error) {
-
-         return res.status(500).json({message : 'Error al crear el post'});
-        }
 }
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,6 +2,9 @@ import express from "express";
 import { getLastPosts, createPost } from "../controllers/postController.js";
 
 
+
+
+
 const postRouter = express.Router();
 
 postRouter.get('/', getLastPosts);
