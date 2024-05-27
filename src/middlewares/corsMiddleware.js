62b97070fe2cf97a6c3ec332d826786ba4d631fb
DenaRi97 <riveraferalmudena@gmail.com>
2024-05-27 11:52:01 +0200

    backend_middleware006_console log added to cors

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -8,10 +8,15 @@ const allowedOrigins = [
 
 const corsOptions = {
     origin: (origin, callback) => {
+        // Agregar log para ver el origen de la solicitud
+        console.log(`Solicitud recibida de origen: ${origin}`);
+
         // Permitir solicitudes de orígenes permitidos o de orígenes nulos
         if (!origin || allowedOrigins.includes(origin)) {
+            console.log('Origen permitido por CORS');
             callback(null, true);
         } else {
+            console.log('Origen no permitido por CORS');
             callback(new Error('Origen no permitido por CORS'));
         }
     },
