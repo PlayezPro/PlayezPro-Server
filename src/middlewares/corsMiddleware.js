1e06ff456a7ea78ba5d1870a82080c4fb0fcba73
DenaRi97 <riveraferalmudena@gmail.com>
2024-05-27 11:24:59 +0200

    backend_middleware005_cors middleware origins

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -3,8 +3,29 @@ import cors from 'cors';
 // Lista de orígenes permitidos
 const allowedOrigins = [
     // 'https://playezpro-client.pages.io', 
+    //sustituir cuando deploy
+    //Frontend
     'http://localhost:4200/',
-    'https://playezpro-server.onrender.com'
+    'http://localhost:4200/auh/signin',
+    'http://localhost:4200/auth/signup',
+    'http://localhost:4200/user',
+    'http://localhost:4200/likes',
+    'http://localhost:4200/post',
+    'http://localhost:4200/skill',
+    'http://localhost:4200/follow',
+    'http://localhost:4200/comment',
+    'http://localhost:4200/detail',
+    //Backend
+    'https://playezpro-server.onrender.com',
+    'https://playezpro-server.onrender.com/auth/signup',
+    'https://playezpro-server.onrender.com/auth/signin',
+    'https://playezpro-server.onrender.com/user',
+    'https://playezpro-server.onrender.com/likes',
+    'https://playezpro-server.onrender.com/posts',
+    'https://playezpro-server.onrender.com/skill',
+    'https://playezpro-server.onrender.com/follow',
+    'https://playezpro-server.onrender.com/comment',
+    'https://playezpro-server.onrender.com/details',
 ];
 
 const corsOptions = {
