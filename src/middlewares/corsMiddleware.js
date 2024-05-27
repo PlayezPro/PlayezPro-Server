c632f0cf81378fa2e624f28960b668b845048f03
DenaRi97 <riveraferalmudena@gmail.com>
2024-05-27 12:14:34 +0200

    backend_middleware007_added deploy origins

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -3,20 +3,28 @@ import cors from 'cors';
 // Lista de orígenes permitidos
 const allowedOrigins = [
     'http://localhost:4200',
+    'https://playezpro-client.netlify.app/',
     'https://playezpro-server.onrender.com',
 ];
 
 const corsOptions = {
     origin: (origin, callback) => {
-        // Agregar log para ver el origen de la solicitud
-        console.log(`Solicitud recibida de origen: ${origin}`);
-
-        // Permitir solicitudes de orígenes permitidos o de orígenes nulos
+        // Log para registrar la solicitud
+        console.log('--- Nueva Solicitud ---');
+        console.log(`Origen de la solicitud: ${origin}`);
+        console.log('Cabeceras de la solicitud:', JSON.stringify(this.headers, null, 2));
+        
+        // Verificación del origen
         if (!origin || allowedOrigins.includes(origin)) {
             console.log('Origen permitido por CORS');
             callback(null, true);
         } else {
-            console.log('Origen no permitido por CORS');
+            // Log detallado para orígenes no permitidos
+            console.log('--- Origen no permitido por CORS ---');
+            console.log(`Origen de la solicitud no permitido: ${origin}`);
+            console.log('Cabeceras de la solicitud:', JSON.stringify(this.headers, null, 2));
+            console.log('Ruta solicitada:', this.path);
+            console.log('Método HTTP:', this.method);
             callback(new Error('Origen no permitido por CORS'));
         }
     },
