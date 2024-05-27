67d45d79ad4d30088bb51383cfe97d53adf92baa
DenaRi97 <riveraferalmudena@gmail.com>
2024-05-27 12:59:06 +0200

    backend_middleware008_hotfix

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,5 +1,12 @@
 import cors from 'cors';
 
+// Lista de orígenes permitidos
+const allowedOrigins = [
+    'http://localhost:4200',
+    'https://playezpro-client.netlify.app/',
+    'https://playezpro-server.onrender.com',
+];
+
 const corsOptions = {
     origin: (origin, callback) => {
         // Log para registrar la solicitud
@@ -22,7 +29,6 @@ const corsOptions = {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
 };
-
 
 const corsMiddleware = cors(corsOptions);
 
