33d9d5811c97be40ac4eb79165e10c0be5055bd8
DenaRi97 <riveraferalmudena@gmail.com>
2024-06-05 09:34:57 +0200

    backend_middleware006_hotfix

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -7,7 +7,9 @@ const postRouter = express.Router();
 postRouter.get('/', getLastPosts);
 postRouter.get('/:id',getPostId)
 postRouter.get('/users/:users_id', showUserPosts)
-postRouter.post('/',fileSizeLimiter, createPost);
+postRouter.post('/',
+// fileSizeLimiter, 
+createPost);
 postRouter.delete('/:id', deletePost);
 postRouter.put('/:id',editBlog)
 // postRouter.get('/rankedLikes', getRankedPosts); // Nueva ruta para los rankings
