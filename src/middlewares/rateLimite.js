3cd9aa23ce877af3894b7150506245dcf9e6cf76
JeanCarlosPerez <jeancperez09@gmail.com>
2024-06-07 11:35:54 +0200

    Backend_rateLimiti Working

--- src/middlewares/rateLimite.js
+++ src/middlewares/rateLimite.js
@@ -3,8 +3,13 @@ import { rateLimit } from 'express-rate-limit';
 export const tokenExpirationTime = 3 * 60 * 60 * 1000
 
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 min
-    max: 3, // Max number of entries to try log in
-    message: 'Too many fail requests, try in again in 15 minutes',
-})
-
+    windowMs: 15 * 60 * 1000, // 15 minutos en milisegundos
+    max: 3, // Número máximo de intentos permitidos por ventana
+    message: {
+      error: 'Too many login attempts, please try again in 15 minutes.',
+      status: 429
+    },
+    headers: true, // Incluir información de limitación de tasa en los encabezados de la respuesta
+    standardHeaders: true, // Enviar encabezados `RateLimit-*` conforme a los estándares
+    legacyHeaders: false, // Deshabilitar los encabezados `X-RateLimit-*` no estándar
+  });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { signIn, singUp } from "../controllers/authController.js";
 import { checkDuplicateUsernameorEmail } from "../middlewares/verifySignup.js";
-import rateLimit from "express-rate-limit";
+import { limiter } from "../middlewares/rateLimite.js";
 
 const router = Router()
 
 router.post('/signup', checkDuplicateUsernameorEmail, singUp)
-router.post('/signin', rateLimit, signIn)
+router.post('/signin', limiter, signIn)
 
 export default router
