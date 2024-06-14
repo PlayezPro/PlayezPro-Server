424b1451ea23a7a16995828d707114f95b026b7a
JeanCarlosPerez <jeancperez09@gmail.com>
2024-06-14 09:38:09 +0200

    Backend_Verifytoken_Middleware VerifyToken working

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -6,27 +6,27 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const SECRET = process.env.JWT_SECRET;
+
+
 export const verifyToken = async (req, res, next) => {
   try {
-    const token = req.header("access");
-    console.log(token)
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!token) return res.status(403).json({ message: "No Token Provider" });
+    if (!token) return res.status(403).json({ message: "No Token Provided" });
 
     const decoded = jwt.verify(token, SECRET);
-
     req.userId = decoded.id;
 
     const user = await Users.findById(req.userId, { password: 0 });
-    console.log(user);
 
     if (!user) return res.status(404).json({ message: 'No User Found' });
 
     next();
   } catch (error) {
+    console.error("Authorization error:", error);
     return res.status(401).json({ message: 'Unauthorized' });
   }
-}
+};
 
 export const isAdmin = async (req, res, next) => {
   const user = await Users.findById(req.userId);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,12 +2,11 @@ import { Router } from "express";
 import { signIn, singUp } from "../controllers/authController.js";
 import { checkDuplicateUsernameorEmail } from "../middlewares/verifySignup.js";
 import { limiter } from "../middlewares/rateLimite.js";
+import { verifyToken } from "../middlewares/authJwt.js";
 
 const router = Router()
 
 router.post('/signup', checkDuplicateUsernameorEmail, singUp)
-router.post('/signin', 
-// limiter, 
-signIn)
+router.post('/signin', verifyToken, limiter, signIn)
 
 export default router
